72b92e6cffac5411a773667f5cd6438a71fab05d
msarar <mohammmedsarar7@gmail.com>
2026-05-17 14:33:29 +0100

    MODFIE BUBBLE SORT

--- js-bubble-sort-analyzer.js
+++ js-bubble-sort-analyzer.js
@@ -5,30 +5,23 @@ const result = bubbleSortAnalyzer([1, 2, 3, 4, 5], comparator);
 console.log(result);
 
 function bubbleSortAnalyzer(arr, comparator) {
-    let swaps = 0;
-    let iterations = 0;
-
-    for (let i = 0; i < arr.length - 1; i++) {
-        let swapped = false;
-
-        for (let j = 0; j < arr.length - 1 - i; j++) {
-            iterations++;
-
-            if (comparator(arr[j], arr[j + 1]) > 0) {
-                swaps++;
-
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-
-                swapped = true;
+    let swp = 0;
+    let itration = 0;
+    for (let i = 0 ; i < arr.length ; i++) {
+        let swped = false 
+        for (let j = 0 ; j < arr.length -1 -i ; j++) {
+            itration++
+            if (comparator(arr[j] , arr[j+1]) > 0) {
+                swp++
+                [arr[j] , arr[j+1]] = [arr[j+1] , arr[j]] 
+                swped = true
             }
         }
-
-        if (!swapped) break;
+        if (!swped) break ;
     }
-
     return {
         sortedArray: arr,
-        iterations,
-        swaps
-    };
+        iterations: itration,
+        swaps: swp
+    }
 }
