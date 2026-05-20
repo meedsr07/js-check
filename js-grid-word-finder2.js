686c8a0bfb736c45bf91b085b77739180a438117
msarar <mohammedsarar7@gmail.com>
2026-05-20 13:02:36 +0100

    new exercice

--- js-grid-word-finder2.js
+++ js-grid-word-finder2.js
@@ -1,39 +1,31 @@
 function gridWordFinder2(grid, word) {
-  if (word === "") return [];
-
-  const results = [];
-  const rows = grid.length;
-  const cols = grid[0].length;
-
-  function matchHorizontal(y, x, i) {
+  if (!word) return []
+  let res = []
+  let rows = grid.length
+  let cols = grid[0].length
+  function checkH(y, x, i = 0) {
     if (i === word.length) return true;
     if (x + i >= cols) return false;
-    if (grid[y][x + i] !== word[i]) return false;
-    return matchHorizontal(y, x, i + 1);
+    if (grid[y][x + i] != word[i]) return false;
+    return checkH(y, x, i + 1)
   }
-
-  function matchVertical(y, x, i) {
+  function checkV(y, x, i = 0) {
     if (i === word.length) return true;
-    if (y + i >= rows) return false;
-    if (grid[y + i][x] !== word[i]) return false;
-    return matchVertical(y, x, i + 1);
+    if (y + i >= rows) return false
+    if (grid[y + i][x] != word[i]) return false
+    return checkV(y, x, i + 1)
   }
-
   function walk(y, x) {
-    if (y === rows) return;
-    if (x === cols) return walk(y + 1, 0);
-
-    if (matchHorizontal(y, x, 0)) {
-      results.push({ x, y, direction: "horizontal" });
+    if (y === rows) return
+    if (x === cols) return walk(y + 1, 0)
+    if (checkH(y, x)) {
+      res.push({ x, y, direction: "horizontal" })
     }
-
-    if (matchVertical(y, x, 0)) {
-      results.push({ x, y, direction: "vertical" });
+    if (checkV(y, x)) {
+      res.push({ x, y, direction: "vertical" })
     }
-
-    return walk(y, x + 1);
+    return walk(y, x + 1)
   }
-
-  walk(0, 0);
-  return results;
+  walk(0, 0)
+  return res
 }
