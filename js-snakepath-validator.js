e8b494970fecdefdb3c752f565ce00bbafbfe51e
msarar <mohammedsarar7@gmail.com>
2026-05-20 11:08:47 +0100

    update the last exercice

--- js-snakepath-validator.js
+++ js-snakepath-validator.js
@@ -16,16 +16,20 @@ console.log(isSnakePath(grid1)); // true
 console.log(isSnakePath(grid2)); // false
 
 function isSnakePath(grid) {
-  const visted = new Set()
+  const visited = new Set()
   function count(row = 0, col = 0) {
-    if (row >= grid.length) return 0
+    if (row >= grid.length) {
+      return 0
+    }
     if (col >= grid[row].length) {
       return count(row + 1, 0)
     }
     return (grid[row][col] === 1 ? 1 : 0) + count(row, col + 1)
   }
   function find(row = 0, col = 0) {
-    if (row >= grid.length) return null
+    if (row >= grid.length) {
+      return null
+    }
     if (col >= grid[row].length) {
       return find(row + 1, 0)
     }
@@ -38,19 +42,19 @@ function isSnakePath(grid) {
     if (row < 0 || col < 0 || row >= grid.length || col >= grid[row].length || grid[row][col] !== 1) {
       return
     }
-    let key = row + ',' + col
-    if (visted.has(key)) return
-    visted.add(key)
-    walk(row - 1, col)
+    let key = row + "," + col
+    if (visited.has(key)) return
+    visited.add(key)
     walk(row + 1, col)
+    walk(row - 1, col)
     walk(row, col + 1)
     walk(row, col - 1)
   }
   const ones = count(0, 0)
-  if ((ones) === 0) return false
+  if (ones === 0) return false
   const start = find(0, 0)
   walk(start[0], start[1])
-  return visted.size === ones
+  return visited.size === ones
 }
 
 
